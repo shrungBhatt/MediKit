cad727be25afb40121bfba76af30031882d86305
Shrung Bhatt <huntjigsaw97@gmail.com>
2018-03-20 22:08:25 +0530

    Added the otp verification mechanism

--- app/src/main/java/com/projects/shrungbhatt/medikit/activities/Activity_Signup.java
+++ app/src/main/java/com/projects/shrungbhatt/medikit/activities/Activity_Signup.java
@@ -2,10 +2,13 @@ package com.projects.shrungbhatt.medikit.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -17,10 +20,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.mobsandgeeks.saripaar.ValidationError;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.ConfirmPassword;
+import com.mobsandgeeks.saripaar.annotation.Digits;
 import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Length;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.mobsandgeeks.saripaar.annotation.Password;
 import com.projects.shrungbhatt.medikit.R;
@@ -31,6 +46,7 @@ import com.projects.shrungbhatt.medikit.util.Utils;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -40,18 +56,23 @@ import java.util.Map;
 public class Activity_Signup extends BaseActivity implements Validator.ValidationListener {
 
     private static final String TAG = "Activity_Signup";
-
+    private static final String KEY_VERIFY_IN_PROGRESS = "key_verify_in_progress";
+    private static final int STATE_INITIALIZED = 1;
+    private static final int STATE_CODE_SENT = 2;
+    private static final int STATE_VERIFY_FAILED = 3;
+    private static final int STATE_VERIFY_SUCCESS = 4;
+    private static final int STATE_SIGNIN_FAILED = 5;
+    private static final int STATE_SIGNIN_SUCCESS = 6;
     @Email
     @NotEmpty
     private EditText mUserEmail;
-
     @Password
     @NotEmpty
     private EditText mUserPassword;
-
     @NotEmpty
     private EditText mUsername;
-
+    @NotEmpty
+    private EditText mPhoneNo;
     @ConfirmPassword
     @NotEmpty
     private EditText mRetypePassword;
@@ -59,6 +80,16 @@ public class Activity_Signup extends BaseActivity implements Validator.Validatio
     private CheckBox mUserTypeCheckBox;
     private String mMacId;
     private Validator mValidator;
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+
+    private boolean mVerificationInProgress = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -88,13 +119,189 @@ public class Activity_Signup extends BaseActivity implements Validator.Validatio
             }
         });
 
+        mPhoneNo = findViewById(R.id.user_sign_up_phone_no);
 
+
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        // Initialize phone auth callbacks
+        // [START phone_auth_callbacks]
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+                // [START_EXCLUDE silent]
+                mVerificationInProgress = false;
+                // [END_EXCLUDE]
+
+                // [START_EXCLUDE silent]
+                // Update the UI and attempt sign in with the phone credential
+                updateUI(STATE_VERIFY_SUCCESS, credential);
+                // [END_EXCLUDE]
+                signInWithPhoneAuthCredential(credential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e);
+                // [START_EXCLUDE silent]
+                mVerificationInProgress = false;
+                // [END_EXCLUDE]
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                    // [START_EXCLUDE]
+
+                    // [END_EXCLUDE]
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                    // [START_EXCLUDE]
+                    Snackbar.make(findViewById(android.R.id.content), "Quota exceeded.",
+                            Snackbar.LENGTH_SHORT).show();
+                    // [END_EXCLUDE]
+                }
+
+                // Show a message and update the UI
+                // [START_EXCLUDE]
+                updateUI(STATE_VERIFY_FAILED);
+                // [END_EXCLUDE]
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+                // Save verification ID and resending token so we can use them later
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+                // [START_EXCLUDE]
+                // Update UI
+                updateUI(STATE_CODE_SENT);
+                // [END_EXCLUDE]
+            }
+        };
+        // [END phone_auth_callbacks]
     }
 
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // [START_EXCLUDE]
+                            updateUI(STATE_SIGNIN_SUCCESS, user);
+                            // [END_EXCLUDE]
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                // [START_EXCLUDE silent]
+                                // [END_EXCLUDE]
+                            }
+                            // [START_EXCLUDE silent]
+                            // Update UI
+                            updateUI(STATE_SIGNIN_FAILED);
+                            // [END_EXCLUDE]
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(int uiState) {
+        updateUI(uiState, mAuth.getCurrentUser(), null);
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            updateUI(STATE_SIGNIN_SUCCESS, user);
+        } else {
+            updateUI(STATE_INITIALIZED);
+        }
+    }
+
+    private void updateUI(int uiState, FirebaseUser user) {
+        updateUI(uiState, user, null);
+    }
+
+    private void updateUI(int uiState, PhoneAuthCredential cred) {
+        updateUI(uiState, null, cred);
+    }
+
+    private void updateUI(int uiState, FirebaseUser user, PhoneAuthCredential cred) {
+        switch (uiState) {
+            case STATE_INITIALIZED:
+                // Initialized state, show only the phone number field and start button
+                showToastMessage("STATE_INITIALIZED");
+                break;
+            case STATE_CODE_SENT:
+                // Code sent state, show the verification field, the
+
+                showToastMessage("STATE_CODE_SENT");
+                break;
+            case STATE_VERIFY_FAILED:
+                // Verification has failed, show all options
+                showToastMessage("STATE_VERIFY_FAILED");
+                break;
+            case STATE_VERIFY_SUCCESS:
+                // Verification has succeeded, proceed to firebase sign in
+                String email = mUserEmail.getText().toString();
+                String password = mUserPassword.getText().toString().trim();
+                String userName = mUsername.getText().toString().trim();
+
+                if (mUserTypeCheckBox.isChecked()) {
+                    registerUser(email, Const.Doctor, password, userName);
+                } else {
+                    registerUser(email, Const.User, password, userName);
+                }
+                showToastMessage("STATE_VERIFY_SUCCESS");
+
+                // Set the verification text based on the credential
+
+
+                break;
+            case STATE_SIGNIN_FAILED:
+                // No-op, handled by sign-in check
+                showToastMessage("STATE_SIGN_IN_FAILED");
+                break;
+            case STATE_SIGNIN_SUCCESS:
+                // Np-op, handled by sign-in check
+                showToastMessage("STATE_SIGN_IN_SUCCESS");
+                break;
+        }
+
+        if (user == null) {
+            // Signed out
+
+
+        } else {
+
+        }
+    }
 
     private void registerUser(final String email, final String userType, final String passWord,
                               final String userName) {
-        showProgressBar(this,TAG);
+        showProgressBar(this, TAG);
         StringRequest stringRequest = new StringRequest(Request.Method.POST,
                 URLGenerator.REGISTER_USER,
                 new Response.Listener<String>() {
@@ -131,7 +338,7 @@ public class Activity_Signup extends BaseActivity implements Validator.Validatio
                 params.put("email", email);
                 params.put("user_type", userType);
                 params.put("password", passWord);
-                params.put("user_name",userName);
+                params.put("user_name", userName);
 
                 return params;
             }
@@ -146,16 +353,20 @@ public class Activity_Signup extends BaseActivity implements Validator.Validatio
 
     @Override
     public void onValidationSucceeded() {
-        String email = mUserEmail.getText().toString();
-        String password = mUserPassword.getText().toString().trim();
-        String userName = mUsername.getText().toString().trim();
+        startPhoneNumberVerification(mPhoneNo.getText().toString().trim());
+    }
 
-        if(mUserTypeCheckBox.isChecked()) {
-            registerUser(email, Const.Doctor, password,userName);
-        }else{
-            registerUser(email,Const.User,password,userName);
-        }
+    private void startPhoneNumberVerification(String phoneNumber) {
+        // [START start_phone_auth]
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                mCallbacks);        // OnVerificationStateChangedCallbacks
+        // [END start_phone_auth]
 
+        mVerificationInProgress = true;
     }
 
     @Override
